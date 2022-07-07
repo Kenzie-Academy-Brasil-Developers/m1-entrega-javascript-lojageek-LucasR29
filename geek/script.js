b5f014960b5fbdd1d3b751f4c6d24d786fcdc985
Lucas <lrigon2904@gmail.com>
2022-07-07 17:23:15 -0300

    finalização

--- geek/script.js
+++ geek/script.js
@@ -1,11 +1,15 @@
+let listFigure = []
+let listPainting = []
+
 function createActionItem(image , name , value){
     let item = {
         imagem: image,
         nome: name,
         valor: value
     }
-    listFigure.push(item)  
+    listFigure.push(item)
 }
+
 
 function createPaintingItem(image , name , value){
     let item = {
@@ -13,10 +17,84 @@ function createPaintingItem(image , name , value){
         nome: name,
         valor: value
     }
-    listPainting.push(item)  
+    listPainting.push(item)
 }
 
-let listFigure = [];
-let listPainting = [];
+createActionItem("../assets/img/actions/animewoman.jpg", 'Anime woman', "R$ 100,00")
+createActionItem("../assets/img/actions/starwarspersonagem.jpg", 'Star Wars', "R$ 150,00")
+createActionItem("../assets/img/actions/dragonballpersonagem.jpg", 'Dragon Ball', "R$ 90,00")
+createActionItem("../assets/img/actions/animewoman.jpg", 'Anime woman', "R$ 100,00")
+createActionItem("../assets/img/actions/starwarspersonagem.jpg", 'Star Wars', "R$ 150,00")
+createActionItem("../assets/img/actions/dragonballpersonagem.jpg", 'Dragon Ball', "R$ 90,00")
+createActionItem("../assets/img/actions/animewoman.jpg", 'Anime woman', "R$ 100,00")
+createActionItem("../assets/img/actions/starwarspersonagem.jpg", 'Star Wars', "R$ 150,00")
+createActionItem("../assets/img/actions/dragonballpersonagem.jpg", 'Dragon Ball', "R$ 90,00")
+createActionItem("../assets/img/actions/animewoman.jpg", 'Anime woman', "R$ 100,00")
+createActionItem("../assets/img/actions/starwarspersonagem.jpg", 'Star Wars', "R$ 150,00")
+createActionItem("../assets/img/actions/dragonballpersonagem.jpg", 'Dragon Ball', "R$ 90,00")
+createActionItem("../assets/img/actions/animewoman.jpg", 'Anime woman', "R$ 100,00")
+createActionItem("../assets/img/actions/starwarspersonagem.jpg", 'Star Wars', "R$ 150,00")
+createActionItem("../assets/img/actions/dragonballpersonagem.jpg", 'Dragon Ball', "R$ 90,00")
+createActionItem("../assets/img/actions/animewoman.jpg", 'Anime woman', "R$ 100,00")
+createActionItem("../assets/img/actions/starwarspersonagem.jpg", 'Star Wars', "R$ 150,00")
+createActionItem("../assets/img/actions/dragonballpersonagem.jpg", 'Dragon Ball', "R$ 90,00")
+createPaintingItem("../assets/img/painting/clock.jpg", 'Clock', 'R$ 150,00')
+createPaintingItem("../assets/img/painting/gamepad.jpg", 'Gamepad', 'R$ 250,00')
+createPaintingItem("../assets/img/painting/personagem.jpg", 'StarWars', 'R$ 300,00')
+createPaintingItem("../assets/img/painting/clock.jpg", 'Clock', 'R$ 150,00')
+createPaintingItem("../assets/img/painting/gamepad.jpg", 'Gamepad', 'R$ 250,00')
+createPaintingItem("../assets/img/painting/personagem.jpg", 'StarWars', 'R$ 300,00')
+createPaintingItem("../assets/img/painting/clock.jpg", 'Clock', 'R$ 150,00')
+createPaintingItem("../assets/img/painting/gamepad.jpg", 'Gamepad', 'R$ 250,00')
+createPaintingItem("../assets/img/painting/personagem.jpg", 'StarWars', 'R$ 300,00')
+createPaintingItem("../assets/img/painting/clock.jpg", 'Clock', 'R$ 150,00')
+createPaintingItem("../assets/img/painting/gamepad.jpg", 'Gamepad', 'R$ 250,00')
+createPaintingItem("../assets/img/painting/personagem.jpg", 'StarWars', 'R$ 300,00')
+createPaintingItem("../assets/img/painting/clock.jpg", 'Clock', 'R$ 150,00')
+createPaintingItem("../assets/img/painting/gamepad.jpg", 'Gamepad', 'R$ 250,00')
+createPaintingItem("../assets/img/painting/personagem.jpg", 'StarWars', 'R$ 300,00')
+createPaintingItem("../assets/img/painting/clock.jpg", 'Clock', 'R$ 150,00')
+createPaintingItem("../assets/img/painting/gamepad.jpg", 'Gamepad', 'R$ 250,00')
+createPaintingItem("../assets/img/painting/personagem.jpg", 'StarWars', 'R$ 300,00')
+createPaintingItem("../assets/img/painting/clock.jpg", 'Clock', 'R$ 150,00')
+createPaintingItem("../assets/img/painting/gamepad.jpg", 'Gamepad', 'R$ 250,00')
+createPaintingItem("../assets/img/painting/personagem.jpg", 'StarWars', 'R$ 300,00')
+createPaintingItem("../assets/img/painting/clock.jpg", 'Clock', 'R$ 150,00')
+createPaintingItem("../assets/img/painting/gamepad.jpg", 'Gamepad', 'R$ 250,00')
+createPaintingItem("../assets/img/painting/personagem.jpg", 'StarWars', 'R$ 300,00')
 
-let secaolistFigure = document.getElementsByClassName('actioFigure')[0]
+
+
+
+
+
+
+let secaolistFigure = document.getElementsByClassName('actionItens')[0]
+let secaolistPainting = document.getElementsByClassName('paintingItens')[0]
+
+console.log(secaolistFigure)
+
+for(let i = 0; i < listFigure.length; i++){
+    let elementoLista = document.createElement('li')
+    let nome = document.createElement('h2')
+    let img = document.createElement('img')
+    let value = document.createElement('span')
+    img.src = listFigure[i].imagem
+    nome.innerText = `${listFigure[i].nome}`
+    value.innerText = `${listFigure[i].valor}`
+    elementoLista.append(img, nome, value)
+    secaolistFigure.appendChild(elementoLista)
+}
+
+
+for(let i = 0; i < listPainting.length; i++){
+    let elementoLista = document.createElement('li')
+    let nome = document.createElement('h2')
+    let img = document.createElement('img')
+    let value = document.createElement('span')
+    img.src = listPainting[i].imagem
+    nome.innerText = `${listPainting[i].nome}`
+    value.innerText = `${listPainting[i].valor}`
+    elementoLista.append(img, nome, value)
+    secaolistPainting.appendChild(elementoLista)
+}
